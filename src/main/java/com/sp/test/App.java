08d851cb61f3eba959e53c0ab0855b93023bfd9a
Shailendra Singh <shailendra.cs09@gmail.com>
2019-04-06 09:47:10 +0530

    initial commit

---
+++ src/main/java/com/sp/test/App.java
@@ -0,0 +1,13 @@
+package com.sp.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
